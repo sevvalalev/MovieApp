52a5be46dfe622b1cba8c4fafcbc8bd155f7cc32
sevvalalev <75833556+sevvalalev@users.noreply.github.com>
2022-10-16 14:37:08 +0300

    Update Constants.swift
    Dont forget add your api key for test purpose

--- MovieApp/Constants/Constants.swift
+++ MovieApp/Constants/Constants.swift
@@ -10,6 +10,6 @@ import Foundation
 struct C {
     struct URL {
         static let imageUrl = "https://image.tmdb.org/t/p/w500"
-        static let movieUrl = "https://api.themoviedb.org/3/movie/550/similar?api_key=3ae9bd6ca029482fd304d1b86f467b17"
+        static let movieUrl = "https://api.themoviedb.org/3/movie/550/similar?api_key={YOUR_API_KEY}"
     }
 }
